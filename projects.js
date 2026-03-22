d040fff80b37c52405541826f43e3ba68cfe6c53
Vinicius <80037970+viniciusbugarin@users.noreply.github.com>
2026-03-22 15:49:07 +0100

    Update projects.js
    updategpt2

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     title: "Calculadora IRPF España",
-    description: "Calcula impuestos automáticamente según normativa española",
+    description: "Herramienta web que permite calcular el impuesto sobre la renta de forma rápida y automática según normativa actual.",
     tech: "JavaScript, HTML, CSS",
     link: "https://viniciusbugarin.github.io/tax-calculator-spain/"
   }
