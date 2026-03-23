2e1e61a8dee6d6f8b732b6cc987b2848602daea1
Vinicius <80037970+viniciusbugarin@users.noreply.github.com>
2026-03-23 16:23:05 +0100

    Update script.js
    update add to reveal

--- script.js
+++ script.js
@@ -40,3 +40,14 @@ form.addEventListener("submit", async function(e) {
     status.innerHTML = "⚠️ Error conexión";
   }
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
