fde40cc856a11bf287741e64c704a63ff28fff4b
Vinicius <80037970+viniciusbugarin@users.noreply.github.com>
2026-03-23 19:45:31 +0100

    Update projects.js
    add ,

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ const projects = [
     description: "Herramienta web que permite calcular el impuesto sobre la renta de forma rápida y automática según normativa actual.",
     tech: "JavaScript, HTML, CSS",
     link: "https://viniciusbugarin.github.io/tax-calculator-spain/"
-  }
+  },
   {
   title: "Calculadora Autónomos España",
   description: "Calcula cuota, impuestos y beneficio real como autónomo",
