d1e242519cc3dcee931df9653f318bc5829592b0
Vinicius <80037970+viniciusbugarin@users.noreply.github.com>
2026-03-30 14:18:08 +0200

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -8,12 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
             <img src="images/VB.png" alt="Vinicius Bugarin" width="50">
           </a>
         </div>
-
-        <ul class="nav-links">
-          <li><a href="index.html">Inicio</a></li>
-          <li><a href="desarrollador-web-barcelona.html">Barcelona</a></li>
-          <li><a href="desarrollador-web-freelance.html">Freelance</a></li>
-        </ul>
+          <a class="nav-links" href="index.html">Inicio</a>
+          <a class="nav-links" href="desarrollador-web-barcelona.html">Barcelona</a>
+          <a class="nav-links" href="desarrollador-web-freelance.html">Freelance</a>
 
       </div>
     </nav>
