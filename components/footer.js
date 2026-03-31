14f2dc46feb9853bf8bcf22856a97dad8f2e4e32
Vinicius <80037970+viniciusbugarin@users.noreply.github.com>
2026-03-31 11:36:36 +0200

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const footer = `
+    <footer class="footer">
+      <div class="footer-container">
+
+        <p>© 2026 Vinicius Bugarin</p>
+
+        <div class="footer-links">
+          <a href="index.html">Inicio</a>
+          <a href="#contact">Contacto</a>
+        </div>
+
+      </div>
+    </footer>
+  `;
+
+  document.getElementById("footer").innerHTML = footer;
+});
